61d3e49bef0ee8da11a889ab5c38b45868319f95
Harsh Raj <harsh.raj8833@gmail.com>
2022-03-14 09:51:25 +0530

    step counting

--- app/src/main/java/com/example/stepcounter/MainActivity.kt
+++ app/src/main/java/com/example/stepcounter/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.example.stepcounter
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.stepcounter.ui.theme.StepCounterTheme
 
 class MainActivity : ComponentActivity(), SensorEventListener{
@@ -23,9 +30,11 @@ class MainActivity : ComponentActivity(), SensorEventListener{
     private var sensorManager: SensorManager? = null
     private var running = false
     private var totalSteps = 0f
-    private var previousTotalSteps = 0f
-    private var currentSteps = 0
+    private var currentSteps = mutableStateOf(totalSteps)
 
+    private val ACTIVITY_RECOGNITION_REQUEST_CODE = 100
+
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -35,41 +44,103 @@ class MainActivity : ComponentActivity(), SensorEventListener{
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Display(steps = currentSteps.toString())
+                    Display()
                 }
             }
+        }
+
+        //check if permission isn't already granted, request the permission
+        if (isPermissionGranted()) {
+            requestPermission()
         }
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
 
     override fun onResume() {
+
         super.onResume()
         running = true
-        val stepSensor: Sensor? = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-        if (stepSensor == null){
+
+        // TYPE_STEP_COUNTER:  A constant describing a step counter sensor
+        // Returns the number of steps taken by the user since the last reboot while activated
+        // This sensor requires permission android.permission.ACTIVITY_RECOGNITION.
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepSensor == null) {
+            // show toast message, if there is no sensor in the device
             Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
-        }
-        else{
+        } else {
+            // register listener with sensorManager
             sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
         }
     }
 
+    override fun onPause() {
+        super.onPause()
+        running = false
+        // unregister listener
+        sensorManager?.unregisterListener(this)
+    }
+
     override fun onSensorChanged(event: SensorEvent?) {
-        if(running){
+
+
+        if (running) { //get the number of steps taken by the user.
             totalSteps = event!!.values[0]
-            currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
-            
+            currentSteps.value = totalSteps
         }
     }
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        println("onAccuracyChanged: Sensor: $sensor; accuracy: $accuracy")
+    }
 
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+                ACTIVITY_RECOGNITION_REQUEST_CODE
+            )
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACTIVITY_RECOGNITION
+        ) != PackageManager.PERMISSION_GRANTED
+    }
+
+    //handle requested permission result(allow or deny)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            ACTIVITY_RECOGNITION_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                ) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                }
+            }
+        }
+    }
+
+    
+
+    @Composable
+    fun Display() {
+        Text(text = currentSteps.value.toInt().toString())
     }
 }
 
 
 
-@Composable
-fun Display(steps: String) {
-    Text(text = steps)
-}
+
+
